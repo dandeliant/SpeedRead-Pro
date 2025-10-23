8a6d820ae28674671ee8d356b813ce9f7c8aa40a
dandeliant <74840084+dandeliant@users.noreply.github.com>
2025-10-23 21:42:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* SpeedRead Pro — Service Worker */
-const REPO_PATH = '/'; // np. '/SpeedRead-Pro/' lub '/' dla user pages
+const REPO_PATH = '/SpeedRead-Pro/'; // np. '/SpeedRead-Pro/' lub '/' dla user pages
 const VERSION = 'v1.0.0';
 const STATIC_CACHE = `sr-static-${VERSION}`;
 const RUNTIME_CACHE = `sr-runtime-${VERSION}`;
